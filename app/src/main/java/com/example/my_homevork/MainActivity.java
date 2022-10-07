dec6b7f75ccf2eb081e88eb7f7f87f4a5cd6c7ca
TolsVA <himtol@yandex.ru>
2022-10-07 20:15:33 +0700

    add+

--- app/src/main/java/com/example/my_homevork/MainActivity.java
+++ app/src/main/java/com/example/my_homevork/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.my_homevork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_second);
+
+                findViewById(R.id.button_2).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        recreate();
+                    }
+                });
+
+            }
+        });
+
     }
 }
